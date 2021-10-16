1d7c75959bc99c7d8e1746cf04e21f1e422c7410
J.Rovshan <rovshan.j_developer@hotmail.com>
2021-10-17 01:07:59 +0400

    Applied ORM

---
+++ Riode Solution/Riode_WebUI/Models/DataContexts/RiodeDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Riode_WebUI.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode_WebUI.Models.DataContexts
+{
+    public class RiodeDbContext : DbContext
+    {
+        public RiodeDbContext()
+            : base()
+        {
+
+        }
+
+        public RiodeDbContext(DbContextOptions options)
+         : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=RiodeWithGit;User Id=sa;Password=query;");
+            }
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ Riode Solution/Riode_WebUI/Models/Entities/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Riode_WebUI.Models.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+    }
+}
